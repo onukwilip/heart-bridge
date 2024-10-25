3c133fc3a80d115dd51fe6fa10c64bfcbba2fcba
onukwilip <onukwilip@gmail.com>
2024-10-25 06:49:37 +0100

    Displayed the option to view public profile from dashboard to orphanage accounts only

--- src/components/profile/ProfileHeader.component.tsx
+++ src/components/profile/ProfileHeader.component.tsx
@@ -21,7 +21,7 @@ const ProfileHeader: FC<{ user?: TUser; loading?: boolean }> = ({
               animation="pulse"
               className="!bg-primary-grey"
               width={"100%"}
-              height={"100%"}
+              height={150}
             />
           </>
         ) : (
