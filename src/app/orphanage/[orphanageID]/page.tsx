cf4e1a9aaf94ed12f60ec093d2718100e44d8bcd
pyfuse <hauwamuktar880@gmail.com>
2024-10-26 06:21:28 +0100

    cleared old work

--- src/app/orphanage/[orphanageID]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "next/navigation";
-import React from "react";
-
-const page = () => {
-  const { orphanageID } = useParams();
-  return <div className="flex flex-col"></div>;
-};
-
-export default page;
--- src/app/orphanage/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="py-4 px-10">{children}</div>;
-};
-
-export default layout;
