368d41fde6205d7c8176d552be91e3c59ccfc35e
pyfuse <hauwamuktar880@gmail.com>
2024-10-28 19:31:50 +0100

    feature: calls modals status update and availability

--- src/components/calls/CallsCard.component.tsx
+++ src/components/calls/CallsCard.component.tsx
@@ -4,17 +4,35 @@ import { useModalContext } from "@/contexts/Modal.context";
 import { TCall, TVisitation } from "@/utils/types";
 import { format } from "date-fns";
 import React from "react";
+import CallDetailsModal from "./CallModal.component";
+import { capitalize } from "@mui/material";
 
 interface VisitationCardProps {
   call: TCall;
 }
 
 const CallsCard: React.FC<VisitationCardProps> = ({ call }) => {
+  const { open_modal } = useModalContext();
+
+  /**
+   * * Function responsible for displaying the visitation details modal
+   */
+  const handleDetailModal = () => {
+    open_modal({
+      children: <CallDetailsModal call={call} />,
+    });
+  };
+
   return (
-    <div className="flex w-full duration-300  flex-col space-y-2 bg-white/10 rounded-xl p-4 border-b-primary border-b-2">
+    <div
+      className="flex w-full duration-300  flex-col space-y-2 bg-white/10 rounded-xl p-4 border-b-primary border-b-2 hover:bg-white/5 cursor-pointer"
+      onClick={handleDetailModal}
+    >
       {/* Status + detail link */}
       <div className="flex justify-between items-center ">
-        <p className="text-golden">Pending</p>
+        <p className={`text-sm font-bold text-${decideColor(call.status)}`}>
+          {capitalize(call.status)}
+        </p>
       </div>
 
       {/* visitor  name */}
@@ -30,3 +48,16 @@ const CallsCard: React.FC<VisitationCardProps> = ({ call }) => {
 };
 
 export default CallsCard;
+
+function decideColor(status: string): string {
+  switch (status) {
+    case "pending":
+      return "golden";
+    case "approved":
+      return "green-500";
+    case "declined":
+      return "red-500";
+    default:
+      return "white";
+  }
+}
--- src/components/visitations/VisitationsTitle.component.tsx
+++ src/components/visitations/VisitationsTitle.component.tsx
@@ -1,23 +1,40 @@
+"use client";
+
 import { MenuItem, Select } from "@mui/material";
 import React from "react";
+import AvailabilitySwitch from "../AvailabilitySwitch.component";
 
 const VisitationsTitle = () => {
+  const [visitationsAvailability, setVisitationsAvailability] =
+    React.useState(true);
+
   return (
     <div className="flex justify-between items-center">
       {/* Title */}
       <h2 className="text-2xl text-white font-bold">Visitations</h2>
 
       <div className="flex items-center gap-x-2">
+        {/* Availability Switch */}
+        <AvailabilitySwitch
+          availability={visitationsAvailability}
+          setAvailability={setVisitationsAvailability}
+        />
+
         {/* Filters for stage */}
         <Select
           variant="outlined"
           defaultValue="Upcoming"
           className="border text-white h-10 "
         >
-          <MenuItem value="Upcoming">Upcoming</MenuItem>
-          {/* <MenuItem value="Pending">Pending</MenuItem> */}
-          <MenuItem value="Completed">Completed</MenuItem>
-          <MenuItem value="Canceled">Canceled</MenuItem>
+          <MenuItem className="menu-item" value="Upcoming">
+            Upcoming
+          </MenuItem>
+          <MenuItem className="menu-item" value="Completed">
+            Completed
+          </MenuItem>
+          <MenuItem className="menu-item" value="Canceled">
+            Canceled
+          </MenuItem>
         </Select>
 
         {/* Status filter */}
@@ -26,9 +43,15 @@ const VisitationsTitle = () => {
           defaultValue="All"
           className="border text-white h-10 "
         >
-          <MenuItem value="All">All</MenuItem>
-          <MenuItem value="Approved">Approved</MenuItem>
-          <MenuItem value="Pending">Pending</MenuItem>
+          <MenuItem className="menu-item" value="All">
+            All
+          </MenuItem>
+          <MenuItem className="menu-item" value="Approved">
+            Approved
+          </MenuItem>
+          <MenuItem className="menu-item" value="Pending">
+            Pending
+          </MenuItem>
         </Select>
       </div>
     </div>
